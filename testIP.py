42615ec4005e09e2dc023ea0a45b70892ff46c0f
MulaRoakee <1422323955@qq.com>
2020-11-29 14:31:05 +0800

    add exe

--- testIP.py
+++ testIP.py
@@ -1,10 +1,5 @@
 import requests
 
-proxy = {
-    'http': 'http://180.94.134.9:63072',
-    'https': 'http://180.94.134.9:63072'
-}
-
 '''head 信息'''
 head = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36', 
         'Connection': 'keep-alive'
